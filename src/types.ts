7be2494ef9376189257ba894197fc190d2e711fe
Steve Hall <hall.steven.g@gmail.com>
2019-01-21 13:22:04 -0700

    use d3-timer type

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,8 @@
+import { Timer } from 'd3-timer'
+
 export type AttrValue = any
 export type EasingFunction = (t: number) => number
 export type Interpolator = (t: number) => void
-
-export type Timer = {
-  stop: () => void
-  restart: (action: Interpolator, delay?: number, time?: number) => void
-}
 
 export type Timing = {
   time: number
