591f28774bf7943e8de22578fb28e0b8d879c16c
Steve Hall <hall.steven.g@gmail.com>
2019-01-21 13:06:16 -0700

    better method names, prettier

--- src/BaseNode.ts
+++ src/BaseNode.ts
@@ -1,24 +1,16 @@
 import { now, timer, timeout } from 'd3-timer'
-import {
-  once,
-  timingDefaults,
-  extend,
-  getTransitionId,
-} from './utils'
-import {
-  Config,
-  Transition
-} from './types'
+import { once, timingDefaults, extend, getTransitionId } from './utils'
+import { Config, Transition } from './types'
 
 class BaseNode {
   state: object
   private transitionData: object
 
   constructor(state?: object) {
-    this.state = state || {} 
+    this.state = state || {}
   }
 
-  animate(config: Array<Config> | Config) {
+  transition(config: Array<Config> | Config) {
     if (Array.isArray(config)) {
       for (let i = 0; i < config.length; i++) {
         this.parse(config[i])
@@ -28,8 +20,18 @@ class BaseNode {
     }
   }
 
-  isAnimating() {
+  isTransitioning() {
     return !!this.transitionData
+  }
+
+  stopTransitions() {
+    const transitions = this.transitionData
+
+    if (transitions) {
+      Object.keys(transitions).forEach(t => {
+        transitions[t].timer.stop()
+      })
+    }
   }
 
   setState(update: object) {
@@ -40,21 +42,25 @@ class BaseNode {
     }
   }
 
-  private parse(config: Config): void {
+  private parse(config: Config) {
     const transitions = { ...config }
-  
+
     const events = transitions.events || {}
-  
+
     if (transitions.events) {
       delete transitions.events
     }
-  
-    const timing = { ...timingDefaults, ...transitions.timing || {}, time: now() }
-  
+
+    const timing = {
+      ...timingDefaults,
+      ...(transitions.timing || {}),
+      time: now(),
+    }
+
     if (transitions.timing) {
       delete transitions.timing
     }
-  
+
     Object.keys(events).forEach(d => {
       if (typeof events[d] !== 'function') {
         throw new Error('Event handlers must be a function')
@@ -62,18 +68,15 @@ class BaseNode {
         events[d] = once(events[d])
       }
     })
-  
-    Object.keys(transitions).forEach((stateKey) => {
+
+    Object.keys(transitions).forEach(stateKey => {
       const tweens = []
       const next = transitions[stateKey]
-  
-      if (
-        typeof next === 'object' &&
-        Array.isArray(next) === false
-      ) {
-        Object.keys(next).forEach((attr) => {
+
+      if (typeof next === 'object' && Array.isArray(next) === false) {
+        Object.keys(next).forEach(attr => {
           const val = next[attr]
-  
+
           if (Array.isArray(val)) {
             if (val.length === 1) {
               tweens.push(this.getTween(attr, val[0], stateKey))
@@ -81,7 +84,7 @@ class BaseNode {
               this.setState((state: object) => {
                 return { [stateKey]: { ...state[stateKey], [attr]: val[0] } }
               })
-  
+
               tweens.push(this.getTween(attr, val[1], stateKey))
             }
           } else if (typeof val === 'function') {
@@ -91,20 +94,20 @@ class BaseNode {
                   return { [stateKey]: { ...state[stateKey], [attr]: val(t) } }
                 })
               }
-  
+
               return kapellmeisterNamespacedTween
             }
-  
+
             tweens.push(getNameSpacedCustomTween)
           } else {
             this.setState((state: object) => {
               return { [stateKey]: { ...state[stateKey], [attr]: val } }
             })
-  
+
             tweens.push(this.getTween(attr, val, stateKey))
           }
         })
-      } else {  
+      } else {
         if (Array.isArray(next)) {
           if (next.length === 1) {
             tweens.push(this.getTween(stateKey, next[0], null))
@@ -117,37 +120,41 @@ class BaseNode {
             const kapellmeisterTween = (t: number) => {
               this.setState({ [stateKey]: next(t) })
             }
-  
+
             return kapellmeisterTween
           }
-  
+
           tweens.push(getCustomTween)
         } else {
           this.setState({ [stateKey]: next })
           tweens.push(this.getTween(stateKey, next, null))
         }
       }
-  
+
       this.update({ stateKey, timing, tweens, events, status: 0 })
     })
   }
 
-  private update(config: Transition) {    
+  private update(transition: Transition) {
     if (!this.transitionData) {
       this.transitionData = {}
     }
-  
-    this.init(getTransitionId(), config)
+
+    this.init(getTransitionId(), transition)
   }
-  
-  private init(id: number, transition: Transition) {  
+
+  private init(id: number, transition: Transition) {
     const n = transition.tweens.length
     const tweens = new Array(n)
-    
+
     const queue = (elapsed: number) => {
       transition.status = 1
-      transition.timer.restart(start, transition.timing.delay, transition.timing.time)
-      
+      transition.timer.restart(
+        start,
+        transition.timing.delay,
+        transition.timing.time,
+      )
+
       if (transition.timing.delay <= elapsed) {
         start(elapsed - transition.timing.delay)
       }
@@ -155,100 +162,107 @@ class BaseNode {
 
     this.transitionData[id] = transition
     transition.timer = timer(queue, 0, transition.timing.time)
-    
+
     const start = (elapsed: number) => {
       if (transition.status !== 1) return stop()
-  
+
       for (const tid in this.transitionData) {
         const t: Transition = this.transitionData[tid]
-  
+
         if (t.stateKey !== transition.stateKey) {
           continue
         }
-  
+
         if (t.status === 3) {
           return timeout(start)
         }
-  
+
         if (t.status === 4) {
           t.status = 6
           t.timer.stop()
-  
+
           if (t.events.interrupt) {
             t.events.interrupt.call(this)
           }
-          
+
           delete this.transitionData[tid]
         } else if (+tid < id) {
           t.status = 6
           t.timer.stop()
-          
+
           delete this.transitionData[tid]
         }
       }
-  
+
       timeout(() => {
         if (transition.status === 3) {
           transition.status = 4
-          transition.timer.restart(tick, transition.timing.delay, transition.timing.time)
+          transition.timer.restart(
+            tick,
+            transition.timing.delay,
+            transition.timing.time,
+          )
           tick(elapsed)
         }
       })
-  
+
       transition.status = 2
-      
+
       if (transition.events.start) {
         transition.events.start.call(this)
       }
-  
+
       if (transition.status !== 2) {
         return
       }
-  
+
       transition.status = 3
-  
+
       let j = -1
-  
+
       for (let i = 0; i < n; ++i) {
         const res = transition.tweens[i].call(this)
-  
+
         if (res) {
           tweens[++j] = res
         }
       }
-  
+
       tweens.length = j + 1
     }
-  
+
     const tick = (elapsed: number) => {
       let t = 1
-  
+
       if (elapsed < transition.timing.duration) {
-        t = transition.timing.ease.call(null, elapsed / transition.timing.duration)
+        t = transition.timing.ease.call(
+          null,
+          elapsed / transition.timing.duration,
+        )
       } else {
         transition.timer.restart(stop)
         transition.status = 5
       }
-  
+
       let i = -1
-  
+
       while (++i < tweens.length) {
         tweens[i].call(null, t)
       }
-  
+
       if (transition.status === 5) {
         if (transition.events.end) {
           transition.events.end.call(this)
         }
-  
+
         stop()
       }
     }
-  
+
     const stop = () => {
       transition.status = 6
       transition.timer.stop()
-  
+
       delete this.transitionData[id]
       for (const i in this.transitionData) return
       delete this.transitionData
@@ -258,15 +272,15 @@ class BaseNode {
   getTween(attr: string, value: any, nameSpace: string) {
     return () => {
       const value0 = nameSpace ? this.state[nameSpace][attr] : this.state[attr]
-  
+
       if (value0 === value) {
         return null
       }
-  
+
       const i = this.getInterpolator(attr, value0, value)
-  
+
       let stateTween: (t: number) => void
-  
+
       if (nameSpace === null) {
         stateTween = (t: number) => {
           this.setState({ [attr]: i(t) })
@@ -279,25 +293,18 @@ class BaseNode {
           })
         }
       }
-  
+
       return stateTween
     }
   }
 
-  getInterpolator(attr: string, begValue: any, endValue: any): (t: number) => any {
+  getInterpolator(
+    attr: string,
+    begValue: any,
+    endValue: any,
+  ): (t: number) => any {
     throw new Error('You must implement getInterpolator.')
   }
-
-  stopAnimating() {
-    const transitions = this.transitionData
-  
-    if (transitions) {
-      Object.keys(transitions).forEach((t) => {
-        transitions[t].timer.stop()
-      })
-    }
-  }
-
 }
 
 export default BaseNode
