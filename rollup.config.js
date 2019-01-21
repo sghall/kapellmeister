c9d1bb7a723941c44af39516b20a522711f0d511
Steve Hall <hall.steven.g@gmail.com>
2019-01-20 23:12:26 -0700

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,77 @@
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import { terser } from 'rollup-plugin-terser'
+
+import pkg from './package.json'
+
+const input = 'src/index.ts'
+const globalName = 'Kapellmeister'
+
+const umd = [
+  {
+    input,
+    output: {
+      file: `build/dist/${pkg.name}.js`,
+      format: 'umd',
+      exports: 'named',
+      name: globalName,
+    },
+    plugins: [
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+        extensions: ['.ts'],
+        presets: [],
+        plugins: [
+          '@babel/plugin-transform-typescript',
+          '@babel/plugin-proposal-class-properties',
+          '@babel/transform-runtime',
+        ],
+      }),
+      nodeResolve({
+        extensions: ['.ts']
+      }),
+      commonjs({
+        include: /node_modules/,
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      sizeSnapshot(),
+    ],
+  },
+  {
+    input,
+    output: {
+      file: `build/dist/${pkg.name}.min.js`,
+      format: 'umd',
+      exports: 'named',
+      name: globalName,
+    },
+    plugins: [
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+        extensions: ['.ts'],
+        presets: [],
+        plugins: [
+          '@babel/plugin-transform-typescript',
+          '@babel/plugin-proposal-class-properties',
+          '@babel/transform-runtime',
+        ],
+      }),
+      nodeResolve({
+        extensions: ['.ts']
+      }),
+      commonjs({
+        include: /node_modules/,
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser(),
+      sizeSnapshot(),
+    ],
+  },
+]
+
+export default umd
