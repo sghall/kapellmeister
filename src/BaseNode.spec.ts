2924eadac4b3cdba51434176cc92a2c5057d309c
Steve Hall <hall.steven.g@gmail.com>
2019-01-21 23:11:11 -0700

    basic test setup

---
+++ src/BaseNode.spec.ts
@@ -0,0 +1,13 @@
+/* eslint-env mocha */
+
+import { assert } from 'chai'
+
+describe('BaseNode', () => {
+  it('should work', () => {
+    assert.strictEqual(true, true, 'should be true')
+  })
+
+  it('should work', () => {
+    assert.strictEqual(true, true, 'should be true')
+  })
+})
---
+++ test/README.md
@@ -0,0 +1,6 @@
+## Tools
+
+- [mocha](https://github.com/mochajs/mocha)
+- [chai](https://github.com/chaijs/chai)
+- [sinon](https://github.com/sinonjs/sinon)
+- [istanbul](http://istanbul-js.org)
